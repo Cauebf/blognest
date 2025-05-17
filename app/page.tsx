717fc33916974f5102de119c1eb3e612b29fb6f4
Cauê Favarim <cauebrolesef@gmail.com>
2025-05-17 20:05:14 -0300

    added fetch data from blogpost

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
+import { prisma } from "./utils/db";
+
+type BlogPost = {
+  title: string;
+  content: string;
+  imageUrl: string;
+  authorImage: string;
+  authorName: string;
+  id: string;
+  creactedAt: Date;
+};
+
+async function getData() {
+  const data: BlogPost[] = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true,
+      creactedAt: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
-    <h1>Hello</h1>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item: BlogPost) => (
+          <div key={item.title} className="bg-white p-4 rounded shadow">
+            <h2 className="text-lg font-semibold mb-2">{item.title}</h2>
+            <p>{item.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
