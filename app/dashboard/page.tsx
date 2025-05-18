1e63dc7729313fe01a0094a97d3f2e3a4e7d2aa6
Cauê Favarim <cauebrolesef@gmail.com>
2025-05-18 00:03:09 -0300

    added create post route and server action

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function DashboardRoute() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="flex items-center justify-between mb-4">
+      <h2 className="text-xl font-medium">Your Blog Articles</h2>
+
+      <Link className={buttonVariants()} href={"/dashboard/create"}>Create Post</Link>
+    </div>
+  );
 }
