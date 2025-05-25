c24ee2c4525212fffd42fda9fd46a5013ce64198
Cauê Favarim <cauebrolesef@gmail.com>
2025-05-24 23:36:53 -0300

    security optimization

--- app/actions.ts
+++ app/actions.ts
@@ -8,6 +8,8 @@ export async function handleSubmission(formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
+  if(!user) return redirect("/api/auth/register");
+
   const title = formData.get("title");
   const content = formData.get("content");
   const url = formData.get("url");
